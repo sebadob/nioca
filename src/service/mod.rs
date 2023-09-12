bf8ccd7d868c2ee1f618c1347de69540d719c732
sd <sebastiandobe@mailbox.org>
2023-09-12 09:22:05 +0200

    factor out x509 chain validation building from init checker

--- src/service/mod.rs
+++ src/service/mod.rs
@@ -1,2 +1,3 @@
 pub mod password_hasher;
 pub mod sealed;
+pub mod x509;
--- src/service/sealed.rs
+++ src/service/sealed.rs
@@ -1,22 +1,18 @@
-use crate::certificates::encryption::{decrypt, encrypt, kdf_danger_static, EncAlg};
-use crate::certificates::x509::cert_from_key_pem;
-use crate::certificates::x509::verification::{
-    validate_x509, x509_der_from_bytes, x509_pem_from_bytes,
-};
+use crate::certificates::encryption::{encrypt, kdf_danger_static, EncAlg};
 use crate::config::{ConfigSealed, Db, EncKeys};
 use crate::constants::{DEV_MODE, INSTANCE_UUID, UNSEAL_RATE_LIMIT};
 use crate::models::api::error_response::{ErrorResponse, ErrorResponseType};
 use crate::models::api::request::{AddMasterShardRequest, InitRequest, UnsealRequest};
-use crate::models::api::response::{
-    CertificateInitInspectResponse, CertificateInspectResponse, InitResponse, SealedStatus,
-};
+use crate::models::api::response::{CertificateInitInspectResponse, InitResponse, SealedStatus};
 use crate::models::db::ca_cert_x509::{CaCertX509Nioca, CaCertX509Root, CaCertX509Type};
 use crate::models::db::enc_key::EncKeyEntity;
 use crate::models::db::master_key::MasterKeyEntity;
 use crate::models::db::sealed::SealedEntity;
 use crate::routes::AppStateSealedExtract;
+use crate::service;
 use crate::service::password_hasher::HashPassword;
-use crate::util::{fingerprint, secure_random};
+use crate::service::x509::CheckedCerts;
+use crate::util::secure_random;
 use ring::digest;
 use sqlx::query;
 use std::env;
@@ -25,7 +21,6 @@ use std::str::FromStr;
 use std::time::Duration;
 use time::OffsetDateTime;
 use tracing::{debug, error, info};
-use utoipa::ToSchema;
 use uuid::Uuid;
 use x509_parser::nom::AsBytes;
 
@@ -195,18 +190,7 @@ pub async fn init(
     })
 }
 
-#[derive(Debug, ToSchema)]
-pub struct CheckedCerts {
-    root_exp: OffsetDateTime,
-    root_cert_pem: String,
-    root_fingerprint: String,
-    nioca_exp: OffsetDateTime,
-    nioca_cert_pem: String,
-    nioca_fingerprint: String,
-    nioca_key_plain: String,
-}
-
-/// Checks the given Nio init values for correctness, validity and consistency.
+/// Checks the given Nioca init values for correctness, validity and consistency.
 pub async fn init_values_check(
     state: &ConfigSealed,
     req: &InitRequest,
@@ -236,131 +220,7 @@ pub async fn init_values_check(
         ));
     }
 
-    // try to serialize the certificates
-
-    // root certificate
-    let root_fingerprint = fingerprint(req.root_pem.trim().as_bytes());
-    let root_pem = x509_pem_from_bytes(req.root_pem.as_bytes()).map_err(|err| {
-        ErrorResponse::new(
-            ErrorResponseType::BadRequest,
-            format!("Bad Root PEM: {}", err.message),
-        )
-    })?;
-    let root_cert = x509_der_from_bytes(root_pem.contents.as_bytes())?;
-    if !root_cert.is_ca() {
-        return Err(ErrorResponse::new(
-            ErrorResponseType::BadRequest,
-            "The given root certificate is not a CA".to_string(),
-        ));
-    }
-    // root certificates are always self-signed
-    root_cert.verify_signature(None).map_err(|err| {
-        let e = ErrorResponse::from(err);
-        ErrorResponse::new(
-            ErrorResponseType::BadRequest,
-            format!("Root Certificate - {}", e.message),
-        )
-    })?;
-    // validate additional parts of the certificate
-    validate_x509(&root_cert).map_err(|_| {
-        ErrorResponse::new(
-            ErrorResponseType::BadRequest,
-            "The given certificate is invalid".to_string(),
-        )
-    })?;
-
-    // intermediate certificate
-    let it_fingerprint = fingerprint(req.it_pem.trim().as_bytes());
-    let it_pem = x509_pem_from_bytes(req.it_pem.as_bytes()).map_err(|err| {
-        ErrorResponse::new(
-            ErrorResponseType::BadRequest,
-            format!("Bad Intermediate PEM: {}", err.message),
-        )
-    })?;
-    let it_cert = x509_der_from_bytes(it_pem.contents.as_bytes())?;
-    if !it_cert.is_ca() {
-        return Err(ErrorResponse::new(
-            ErrorResponseType::BadRequest,
-            "The given intermediate certificate is not a CA".to_string(),
-        ));
-    }
-    // verify the signature with the roots public key
-    it_cert
-        .verify_signature(Some(root_cert.public_key()))
-        .map_err(|err| {
-            let e = ErrorResponse::from(err);
-            ErrorResponse::new(
-                ErrorResponseType::BadRequest,
-                format!("Intermediate Certificate - {}", e.message),
-            )
-        })?;
-    // validate additional parts of the certificate
-    validate_x509(&it_cert).map_err(|_| {
-        ErrorResponse::new(
-            ErrorResponseType::BadRequest,
-            "The given certificate is invalid".to_string(),
-        )
-    })?;
-
-    // try to decode the private key
-    let key_bytes = match hex::decode(req.it_key.trim()) {
-        Ok(b) => b,
-        Err(err) => {
-            error!("{}", err);
-            return Err(ErrorResponse::new(
-                ErrorResponseType::BadRequest,
-                "Cannot decode the intermediate key from HEX format".to_string(),
-            ));
-        }
-    };
-    // try to decrypt it with the given password
-    let secret = kdf_danger_static(req.it_password.as_bytes()).await?;
-    let key_plain = match decrypt(&key_bytes, secret.as_ref()) {
-        Ok(k) => k,
-        Err(err) => {
-            error!("{}", err.message);
-            return Err(ErrorResponse::new(
-                ErrorResponseType::BadRequest,
-                "Cannot decrypt the Intermediate Private Key".to_string(),
-            ));
-        }
-    };
-    let nioca_key_plain = match String::from_utf8(key_plain) {
-        Ok(k) => k,
-        Err(err) => {
-            error!("{}", err);
-            return Err(ErrorResponse::new(
-                ErrorResponseType::BadRequest,
-                "Cannot parse the Intermediate Private Key to String".to_string(),
-            ));
-        }
-    };
-    // try to rebuild the full intermediate certificate
-    let _it_cert_full = cert_from_key_pem(&nioca_key_plain, &req.it_pem).await?;
-
-    // everything is valid
-    let checked_certs = CheckedCerts {
-        root_exp: root_cert.validity.not_after.to_datetime(),
-        root_cert_pem: req.root_pem.trim().to_string(),
-        root_fingerprint,
-        nioca_exp: it_cert.validity.not_after.to_datetime(),
-        nioca_cert_pem: req.it_pem.trim().to_string(),
-        nioca_fingerprint: it_fingerprint,
-        nioca_key_plain,
-    };
-
-    // build the API response
-    let name = "default".to_string();
-    let resp = CertificateInitInspectResponse {
-        root: CertificateInspectResponse::from_certificate(
-            Uuid::default(),
-            name.clone(),
-            root_cert,
-        ),
-        intermediate: CertificateInspectResponse::from_certificate(Uuid::default(), name, it_cert),
-    };
-
-    Ok((checked_certs, resp))
+    service::x509::x509_ca_validate(&req.root_pem, &req.it_pem, &req.it_key, &req.it_pem).await
 }
 
 /// Checks a given master shard key against the check hashes from the database and saves the shard
