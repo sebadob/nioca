29dc22e1f85f9984304a1aa10d1b23c8efd06ecb
sd <sebastiandobe@mailbox.org>
2023-09-18 13:21:14 +0200

    DB CRUD

--- src/models/db/groups.rs
+++ src/models/db/groups.rs
@@ -1,6 +1,6 @@
 use crate::config::Db;
 use crate::models::api::error_response::ErrorResponse;
-use crate::models::api::request::GroupUpdateRequest;
+use crate::models::api::request::{GroupCreateRequest, GroupUpdateRequest};
 use serde::{Deserialize, Serialize};
 use sqlx::{query, query_as};
 use uuid::Uuid;
@@ -48,9 +48,32 @@ impl GroupEntity {
         Ok(res)
     }
 
-    pub async fn update(req: GroupUpdateRequest) -> Result<(), ErrorResponse> {
+    pub async fn insert(req: GroupCreateRequest) -> Result<(), ErrorResponse> {
         query!(
-            "UPDATE groups SET name = $1, enabled = $2, ca_ssh = $3, ca_x509 = $4 WHERE id = $5",
+            r#"INSERT INTO groups (id, name, enabled, ca_ssh, ca_x509, ca_x509_typ)
+            VALUES ($1, $2, true, $3, $4, 'certificate')"#,
+            Uuid::new_v4(),
+            req.name,
+            req.ca_ssh,
+            req.ca_x509,
+        )
+        .execute(Db::conn())
+        .await?;
+        Ok(())
+    }
+
+    pub async fn delete(id: &Uuid) -> Result<(), ErrorResponse> {
+        query!("DELETE FROM groups WHERE id = $1", id,)
+            .execute(Db::conn())
+            .await?;
+        Ok(())
+    }
+
+    pub async fn update(req: GroupUpdateRequest) -> Result<(), ErrorResponse> {
+        // the AND name != 'default' is necessary to never change the name of the default groups, which would break
+        // some logic
+        query!(
+            "UPDATE groups SET name = $1, enabled = $2, ca_ssh = $3, ca_x509 = $4 WHERE id = $5 AND name != 'default'",
             req.name,
             req.enabled,
             req.ca_ssh,
