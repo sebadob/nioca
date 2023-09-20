6ce2d798350d871f89ae9d962eddefe4791c14c4
sd <sebastiandobe@mailbox.org>
2023-09-20 10:10:25 +0200

    aria roles

--- frontend/src/components/admin/clientsX509/ClientTileAddNew.svelte
+++ frontend/src/components/admin/clientsX509/ClientTileAddNew.svelte
@@ -169,6 +169,8 @@
                 <div class="flexRow">
                     {uuid}
                     <div
+                            role="button"
+                            tabindex="0"
                             class="btn"
                             on:click={copy}
                             on:keypress={copy}
